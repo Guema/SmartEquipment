4c070bbc74581e3d541c849f924b3d22579edab5
Guema <floorent1993@gmail.com>
2017-02-17 14:02:16 +0100

    Changed addon name and testing some other features

--- Core.lua
+++ Core.lua
@@ -1,20 +1,55 @@
+autoGS = LibStub("AceAddon-3.0"):NewAddon("AutoGearSwap", "AceConsole-3.0", "AceEvent-3.0")
 
-autoGC = LibStub("AceAddon-3.0"):NewAddon("AutoGearChanger", "AceConsole-3.0", "AceEvent-3.0")
+local options = {
+    name = "AutoGearSwap",
+    handler = autoGS,
+    type = 'group',
+    args = {
+        msg = {
+            type = "input",
+            name = "Message",
+            desc = "The message to be displayed when you get home.",
+            usage = "<Your message>",
+            get = "GetMessage",
+            set = "SetMessage",
+        },
+    },
+}
 
-function autoGC:OnInitialize()
+autoGS.message = "Default Message"
+
+function autoGS:OnInitialize()
     -- Called when the addon is loaded
-    self:Print("Saucisse !")
+    LibStub("AceConfig-3.0"):RegisterOptionsTable("AutoGearSwap", options, {"autogearswap", "autogs", "ags"})
+    self:RegisterChatCommand("setext", "ChatCommand")
 end
 
-function autoGC:OnEnable()
+function autoGS:OnEnable()
     -- Called when the addon in enabled
     self:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
 end
 
-function autoGC:OnDisable()
+function autoGS:OnDisable()
     -- Called when the addon is disabled
 end
 
-function autoGC:PLAYER_SPECIALIZATION_CHANGED()
-    self:Print("Spec changed for %spec_name%. Gear was automatically changed for %gear_set_name%")
+function autoGS:PLAYER_SPECIALIZATION_CHANGED()
+    self:Print(self.message)
+end
+
+
+function autoGS:GetMessage(info)
+    return self.message
+end
+
+function autoGS:SetMessage(info, newValue)
+    self.message = newValue
+end
+
+function autoGS:ChatCommand(input)
+    if not input or input:trim() == "" then
+        -- InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
+    else
+        -- LibStub("AceConfigCmd-3.0"):HandleCommand("wh", "WelcomeHome", input)
+    end
 end
